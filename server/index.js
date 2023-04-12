d6a68ccaf98ad88816f1a5993b008b1c2919eb6f
Riya Rajput <78618457+reerajput930@users.noreply.github.com>
2023-04-12 18:29:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT ="5000"
+const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose')
 const cors = require('cors')
 const missRouter = require("./routes/missRoutes")
