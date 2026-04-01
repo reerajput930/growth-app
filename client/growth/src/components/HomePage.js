bd38fd35ee999d751d52fbc15ed4e2e3fe0fe22e
Riya . <78618457+reerajput930@users.noreply.github.com>
2026-04-01 17:52:04 -0400

    Update HomePage.js

--- client/growth/src/components/HomePage.js
+++ client/growth/src/components/HomePage.js
@@ -5,7 +5,7 @@ export default function HomePage(){
     return(
         <div className="flex justify-center items-center mt-20 ">
             <div className="gender--heading">
-                <Link to={"/mr_currenttask"}><h2 className=" cursor-pointer mr--block text-3xl  mr-2 font-semibold inline">KARAN</h2></Link>
+                <Link to={"/mr_currenttask"}><h2 className=" cursor-pointer mr--block text-3xl  mr-2 font-semibold inline">SMIT</h2></Link>
                 <span className="relative top-1 text-5xl">/</span>
                 <Link to={"/miss_currenttask"}> <h2 className=" cursor-pointer miss--block mr--block ml-2 text-3xl font-semibold inline">RIYA</h2></Link>
                 
